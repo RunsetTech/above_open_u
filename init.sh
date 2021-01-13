fd2f0a5928c7cf4c28c26f747ae48a9432283329
RunsetTech <52034765+RunsetTech@users.noreply.github.com>
2021-01-13 18:28:47 +0330

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+cat > ghostinit.sh <<EOF
+sudo systemctl disable ghostinit
+wget https://raw.githubusercontent.com/RunsetTech/above_open_u/master/bottom.sh && sudo bash bottom.sh
+EOF
+
+sudo cp ghostinit.sh /usr/bin/ghostinit.sh
+sudo chmod +x /usr/bin/ghostinit.sh
+
+cat > ghostinit.service <<EOF
+[Unit]
+Description=Ghost Init Service
+[Service]
+Type=simple
+ExecStart=/bin/bash /usr/bin/ghostinit.sh
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo cp ghostinit.service /etc/systemd/system/ghostinit.service
+sudo chmod 644 /etc/systemd/system/ghostinit.service
+
+sudo systemctl start ghostinit
+sudo systemctl enable ghostinit
